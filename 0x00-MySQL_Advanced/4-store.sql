4b04b72d870e07abbe14373efd16cfb3092bfe7f
Innocent9712 <chukwuemeka140@gmail.com>
2023-01-13 00:10:16 +0100

    [fix] checker error task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,4 @@
--- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
--- Column names must be: band_name and lifespan (in years)
--- use attributes formed and split for computing the lifespan
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
-metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+-- Script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;

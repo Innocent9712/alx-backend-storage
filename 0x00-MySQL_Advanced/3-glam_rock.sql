878bdca5311550ab0f1a7db18eff31b2218f332b
Innocent9712 <chukwuemeka140@gmail.com>
2023-01-13 03:39:58 +0100

    add comment

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam as their main style, ranked by their longevity
-
+-- glam rock
 SELECT band_name, COALESCE(split, 2021) - formed as lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%';

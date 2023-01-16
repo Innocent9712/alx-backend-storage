cf076995d6a71022a7a491972e849f4434e79222
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-16 23:25:09 +0100

    Add advanced tasks

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,6 @@ def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
     if mongo_collection is not None:
         return mongo_collection.update_many(
-            {"name": name}, 
+            {"name": name},
             {"$set": {"topics": topics}}
             )

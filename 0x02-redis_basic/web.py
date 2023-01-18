6c351531d532655b98a09b8002952d6c65e7d90d
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-18 14:44:46 +0100

    Add advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
-"""5. Implementing an expiring web cache and tracker"""
-import requests
-from typing import Callable
+"""
+create a web cach
+"""
 import redis
+import requests
+rc = redis.Redis()
+count = 0
 
 
-def counter(func: Callable) -> Callable:
-    """Counts and stores the number of times a url is visited
-    caches the count for 10secs"""
-    def wrapper(url: str) -> Callable:
-        newConn = redis.Redis()
-        newConn.incrby(f"count:{url}", amount=1)
-        newConn.expire(f"count:{url}", 10)
-        return func(url)
-    return wrapper
-
-
-@counter
 def get_page(url: str) -> str:
-    """In this tasks, we will implement a get_page function
-    The core of the function is very simple.
-    It uses the requests module to obtain the HTML content of
-    a particular URL and returns it."""
-    return requests.get(url).content
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
 
 if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
+    get_page('http://slowwly.robertomurray.co.uk')
